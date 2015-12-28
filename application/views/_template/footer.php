5622ebf2941369244d7fe66bcb294dbc384acacf
Minzer-Alexey <minzer.alexey@gmail.com>
2015-12-28 20:27:12 +0300

    fix footer links

--- application/views/_template/footer.php
+++ application/views/_template/footer.php
@@ -4,12 +4,14 @@
             <div class="col-md-3">
                 <h3>Каталог</h3>
                 <p>
-                    <a href="#">Розетки и выключатели</a><br />
-                    <a href="#">Лампочки</a><br />
-                    <a href="#">Кабель и провод</a><br />
-                    <a href="#">Светотехника</a> <br/>
-                    <a href="#">Стабилизаторы</a> <br/>
-                    <a href="#">Счётчики</a>
+                    <?php
+                        $ph->link('Розетки и выключатели', '/catalog')->single_tag('br')
+                            ->link('Лампочки', '/catalog')->single_tag('br')
+                            ->link('Кабель и провод', '/catalog')->single_tag('br')
+                            ->link('Светотехника', '/catalog')->single_tag('br')
+                            ->link('Стабилизаторы', '/catalog')->single_tag('br')
+                            ->link('Счётчики', '/catalog')->single_tag('br')
+                    ?>
                 </p>
             </div>
             <div class="col-md-3">
@@ -34,14 +36,14 @@
             </div>
             <div class="col-md-3">
                 <h3>График работы</h3>
-                Понедельник     Выходной <br/>
-                Вторник 08:00 — 18:00 <br/>
-                Среда   08:00 — 18:00 <br/>
-                Четверг 08:00 — 18:00 <br/>
-                Пятница 08:00 — 18:00 <br/>
-                Суббота 08:00 — 18:00 <br/>
-                Воскресенье     08:00 — 15:00 <br/>
-                для того чтобы заказать товар на завтра звоните до 20-00 <br/>
+                Понедельник     Выходной <br>
+                Вторник 08:00 — 18:00 <br>
+                Среда   08:00 — 18:00 <br>
+                Четверг 08:00 — 18:00 <br>
+                Пятница 08:00 — 18:00 <br>
+                Суббота 08:00 — 18:00 <br>
+                Воскресенье     08:00 — 15:00 <br>
+                для того чтобы заказать товар на завтра звоните до 20-00 <br>
             </div>
             <div class="col-md-3">
                 <h3>Контакты</h3>
@@ -49,22 +51,22 @@
                     <li>
                         <span class="glyphicon glyphicon-phone-alt"></span>
                         <span class="field">Телефон:</span>
-                        <br />
-                        8-029-850-40-85 / 8-044-461-09-06
+                        <br>
+                        <?php $ph->link('8 029 850 40 85', '/contacts')
+                            ->single_tag('br')
+                            ->link('8 044 461 09 06', '/contacts') ?>
                     </li>
                     <li>
                         <span class="glyphicon glyphicon-envelope"></span>
                         <span class="field">Email:</span>
-                        <br />
-                        north-s@tut.by
+                        <br>
+                        <?php $ph->link('north-s@tut.by', '/contacts') ?>
                     </li>
                     <li>
                         <span class="glyphicon glyphicon-home"></span>
                         <span class="field">Адрес:</span>
-                        <br />
-                        г.Минск
-                        ул. Малинина 35а <br/>
-                        павильон 2/7-50
+                        <br>
+                        <?php $ph->link('г.Минск ул. Малинина 35а <br> павильон 2/7-50', '/contacts') ?>
                     </li>
                 </ul>
             </div>
