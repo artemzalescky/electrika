90df66cb8ca4042972296ff35c96e4e77906c623
goldmagnat <artemzalescky@gmail.com>
2016-01-04 12:26:31 +0300

    about......НЕ ПОЛУЧАЕТСЯ УМЕНЬШИТЬ ФОТО КОГДА УМЕНЬШАЕШЬ РАЗМЕР ЭКРАНА((

--- application/views/about.php
+++ application/views/about.php
@@ -1,1 +1,5 @@
-О компании
+<div class="container">
+    <div class="col-lg-12" style="margin-top: 0px; height: 1500px; max-width: 100px">
+        <?php $ph->image('about/ip1.png')?>
+    </div>
+</div>
