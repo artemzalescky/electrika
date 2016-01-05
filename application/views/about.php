9d7d8a82965635e8fdf31fe347c5c264c118364a
Minzer-Alexey <minzer.alexey@gmail.com>
2016-01-05 09:09:39 +0300

    картинка масштабируется

--- application/views/about.php
+++ application/views/about.php
@@ -1,5 +1,1 @@
-<div class="container">
-    <div class="col-lg-12" style="margin-top: 0px; height: 1500px; max-width: 100px">
-        <?php $ph->image('about/ip1.png')?>
-    </div>
-</div>
+<?php $ph->image('/about/ip1.png', ['style' => 'width: 100%']);
